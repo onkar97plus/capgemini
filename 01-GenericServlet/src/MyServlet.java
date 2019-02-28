5f315392610aff599554cd8d30143f3e5cf873c7
onkar97plus <47916581+onkar97plus@users.noreply.github.com>
2019-02-28 12:27:59 +0530

    Add files via upload

---
+++ 01-GenericServlet/src/MyServlet.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+public class MyServlet extends GenericServlet{
+
+	public void service(ServletRequest request,ServletResponse response)throws
+	ServletException,IOException
+	{
+		PrintWriter out=response.getWriter();
+		
+		out.println("Hello WebServlet");
+		
+		System.out.println("Hello Servlet");
+	}
+
+}
